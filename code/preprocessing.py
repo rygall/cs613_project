16a9b6c324afbc250879aa793c01df92a94b4017
rygall <91382298+rygall@users.noreply.github.com>
2023-06-06 00:29:33 -0400

    Add files via upload

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,71 +1,62 @@
 import numpy as np
 import csv
 import sys
-#np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
 
 
 # BEGINNING OF DATA PREPROCESSING
 
-# extracting data and pre processing it for ease of use in the algorithm
 data = []
-with open('dataset/train.csv') as realtordata:
+with open('train.csv') as realtordata:
+
     real_read = csv.reader(realtordata)
-    count = 0
+    
+    # remove first row and column from data
+    temp_data = []
     for row in real_read:
-        print(row)
-        # skip column labels row
-        if count == 0:
-            count = count + 1
-            continue
+        temp_data.append(row)
+    temp_data = np.array(temp_data)
+    temp_data = temp_data[1:, 1:]
+
+    # determine the total number of unique possible values for each feature
+    # also store the possible values that those features can take on
+    num_of_unique_values = np.zeros(np.size(temp_data, axis=1))
+    possible_values = []
+    temp_data_transpose = np.transpose(temp_data)
+    for index in range(0, (np.size(temp_data_transpose, axis=0))):
+        junk, counts = np.unique(temp_data_transpose[index], return_counts=True)
+        num_of_unique_values[index] = len(counts)
+        possible_values.append(junk)
+
+
+    count = 0
+    for row in temp_data:
         
         # we use the temp array to preprocess each row
-        temp = np.array(row[0:10])
-        print(temp)
+        temp = []
 
+        # look through each value in the row
+        for index in range(0, len(row)):
+            value = None
+            try:
+                value = int(row[index])
+                temp.append(value)
+            except:
+                value = row[index]
+                print(value)
+                different_values = int(num_of_unique_values[index])
+                array = np.zeros(different_values)
+                for j in range(0, len(possible_values[index])):
+                    if value == possible_values[index][j]:
+                        array[j] = 1
+                for x in array:
+                    temp.append(x)
 
-
-        '''
-        # convert gender into binary feature
-        if temp[1] == 'male':
-            temp[1] = 1
-        else:
-            temp[1] = 0
-
-        # make smoker a binary feature
-        if temp[4] == 'yes':
-            temp[4] = 1
-        else:
-            temp[4] = 0
-
-        # make region into 4 binary features
-        # one-hot encoding
-        region = temp[5]
-        temp = np.delete(temp, 5)
-        temp = np.append(temp, 0)
-        temp = np.append(temp, 0)
-        temp = np.append(temp, 0)
-        temp = np.append(temp, 0)
-        if region == 'northeast':
-            temp[6] = 1
-        if region == 'northwest':
-            temp[7] = 1
-        if region == 'southeast':
-            temp[8] = 1
-        if region == 'southwest':
-            temp[9] = 1
-    
-        # moving charges to the end of the array
-        temp = np.append(temp, temp[5])
-        temp = np.delete(temp, 5)
-        '''
-
-        # inserting a bias feature into the front of the array
-        temp = np.insert(temp, 0, 1)
-
+        print(np.shape(temp))
         # append the new array to the data array
         data.append(temp)
-        count = count + 1
 
-data = np.array(data, dtype=float)
+print(data)
+data = np.array(data, dtype=int)
 
 # END OF DATA PREPROCESSING
