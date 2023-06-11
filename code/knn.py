5508aacf9fec4b6cec139a9b92f5f8a9dff60885
rygall <91382298+rygall@users.noreply.github.com>
2023-06-11 11:20:27 -0400

    Add files via upload

---
+++ code/knn.py
@@ -0,0 +1,91 @@
+import numpy as np
+from collections import deque
+import sys
+import math
+import preprocessing as data
+
+# k-nearest neighbors function
+def myKNN(training_data, training_classes, validation_data, k):
+
+    # loop through each validation sample and determine nearest neighbor class
+    validation_vector = []
+    for validation_sample in validation_data:
+
+        # linked list to store nearest neighbors
+        linked_list = deque()
+        for x in range(0, k):
+            linked_list.append([sys.maxsize, 1])
+        
+
+        # get nearest neighbors
+        for i in range(0, len(training_data)):
+            squared_euclidian = np.subtract(validation_sample, training_data[i])
+            squared_euclidian = np.power(squared_euclidian, 2)
+            squared_euclidian = np.sum(squared_euclidian) 
+            euclidian = np.sqrt(squared_euclidian)
+            new_node = [euclidian, training_classes[i]]
+            linked_list.append(new_node)
+            max_node = new_node
+            for node in linked_list:
+                if node[0] > max_node[0]:
+                    max_node = node
+            linked_list.remove(max_node)
+
+        # calculate mean prediction
+        total = 0
+        for node in linked_list:
+            total += node[1]
+        prediction = total / len(linked_list)
+
+        # determine highest count class
+        validation_vector.append(prediction)
+
+    
+    return validation_vector
+
+
+# DATA PREPROCESSING
+# opening and reading data from data file
+data = data.getContinousData()
+
+# random shuffle data
+np.random.seed(0)
+np.random.shuffle(data)
+
+# splitting training and validation data
+split_index = int((2/3) * np.size(data, axis=0))
+training_data = data[:split_index, :-1]
+training_classes = data[:split_index, -1]
+validation_data = data[split_index:, :-1]
+validation_classes = data[split_index:, -1]
+
+# get validation vector
+# SET K VALUE HERE
+k = 5
+validation_vector = myKNN(training_data, training_classes, validation_data, k)
+
+# RMSE
+total_squared_error = 0
+for i in range(0, len(validation_vector)):
+    difference = validation_classes[i] - validation_vector[i]
+    squared_error = math.pow(difference, 2)
+    total_squared_error += squared_error
+batch_se = total_squared_error / len(validation_vector)
+rmse = math.sqrt(batch_se)
+print("\nRMSE\n", rmse)
+
+# SMAPE
+total = 0
+for i in range(0, len(validation_vector)):
+    prediction = validation_vector[i]
+    actual = validation_classes[i]
+    difference = abs(actual - prediction)
+    sample_smape = difference / (abs(actual) + abs(prediction))
+    total += sample_smape
+smape = (total / len(validation_vector))
+print("\nSMAPE\n", smape)
+print()
+
+
+
+
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,76 +1,86 @@
 import numpy as np
 import csv
 import sys
-np.set_printoptions(threshold=sys.maxsize)
 
 
-# BEGINNING OF DATA PREPROCESSING
+def getContinousData():
+    data = []
+    with open('train.csv') as realtordata:
 
-data = []
-with open('train.csv') as realtordata:
-
-    # read in data
-    real_read = csv.reader(realtordata)
-    
-    # remove first row and column from data
-    temp_data = []
-    for row in real_read:
-        temp_data.append(row)
-    temp_data = np.array(temp_data)
-    temp_data = temp_data[1:, 1:]
-
-    # determine the total number of unique possible values for each feature
-    # also store the possible values that those features can take on
-    num_of_unique_values = np.zeros(np.size(temp_data, axis=1))
-    possible_values = []
-    temp_data_transpose = np.transpose(temp_data)
-    for index in range(0, (np.size(temp_data_transpose, axis=0))):
-        junk, counts = np.unique(temp_data_transpose[index], return_counts=True)
-        num_of_unique_values[index] = len(counts)
-        possible_values.append(junk)
-
-    # loop through each row of data
-    for row in temp_data:
+        # read in data
+        real_read = csv.reader(realtordata)
         
-        # use temp array to store the update feature values for this row
-        temp = []
+        # remove first row and column from data
+        temp_data = []
+        for row in real_read:
+            temp_data.append(row)
+        temp_data = np.array(temp_data)
+        temp_data = temp_data[1:, 1:]
 
-        # look through each value in the row
-        for index in range(0, len(row)):
+        # determine the total number of unique possible values for each feature
+        # also store the possible values that those features can take on
+        num_of_unique_values = np.zeros(np.size(temp_data, axis=1))
+        possible_values = []
+        temp_data_transpose = np.transpose(temp_data)
+        for index in range(0, (np.size(temp_data_transpose, axis=0))):
+            junk, counts = np.unique(temp_data_transpose[index], return_counts=True)
+            num_of_unique_values[index] = len(counts)
+            possible_values.append(junk)
 
-            value = None
+        # loop through each row of data
+        for row in temp_data:
+            
+            # use temp array to store the update feature values for this row
+            temp = []
 
-            '''
-            THIS COULD POTENITALLY BE AN EASIER SOLUTION BUT REMOVES WHOLE SAMPLES RATHER THAN FEATURES
-            if value == 'NA':
-                break
-            '''
-            if index == 2: #LotFrontage
-                continue
-            if index == 25: # MasVnrArea
-                continue
-            if index == 58: # GarageYrBlt
-                continue
+            # look through each value in the row
+            for index in range(0, len(row)):
 
-            # if it can be converted to an integer, append it to the temp array
-            try:
-                value = int(row[index])
-                temp.append(value)
-            # if its a string, one hot encode it and then append the 1xD row to the temp array, value by value
-            except:
-                value = row[index]
-                different_values = int(num_of_unique_values[index])
-                array = np.zeros(different_values)
-                for j in range(0, len(possible_values[index])):
-                    if value == possible_values[index][j]:
-                        array[j] = 1
-                for x in array:
-                    temp.append(x)
-        
-        # append the new array to the data array
-        data.append(temp)
+                value = None
 
-data = np.array(data, dtype=int)
-print(data)
+                '''
+                THIS COULD POTENITALLY BE AN EASIER SOLUTION BUT REMOVES WHOLE SAMPLES RATHER THAN FEATURES
+                if value == 'NA':
+                    break
+                '''
+                if index == 2: #LotFrontage
+                    continue
+                if index == 25: # MasVnrArea
+                    continue
+                if index == 58: # GarageYrBlt
+                    continue
 
-# END OF DATA PREPROCESSING
+                # if it can be converted to an integer, append it to the temp array
+                try:
+                    value = int(row[index])
+                    temp.append(value)
+                # if its a string, one hot encode it and then append the 1xD row to the temp array, value by value
+                except:
+                    value = row[index]
+                    different_values = int(num_of_unique_values[index])
+                    array = np.zeros(different_values)
+                    for j in range(0, len(possible_values[index])):
+                        if value == possible_values[index][j]:
+                            array[j] = 1
+                    for x in array:
+                        temp.append(x)
+            
+            # append the new array to the data array
+            data.append(temp)
+
+    # separate classes from data
+    data = np.array(data, dtype=int)
+    classes = data[:, -1:]
+    data = data[:, :-1]
+
+    # z-scoring training data
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0, ddof=1)
+    data = (data - mean) / std
+
+    # append classes back onto data
+    data = np.append(data, classes, axis=1)
+
+    return data
+
+getContinousData()
