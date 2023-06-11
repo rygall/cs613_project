74a9f1823fdcb8253aacbb8c5edc88f006260d17
rygall <91382298+rygall@users.noreply.github.com>
2023-06-11 14:26:38 -0400

    Add files via upload

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,9 +1,15 @@
 import numpy as np
 import csv
 import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 
-def getContinousData():
+def getBinWidth(data, feature_index, num_of_bins):
+
+    return getBinWidth
+
+
+def getContinousData(zscore=True):
     data = []
     with open('train.csv') as realtordata:
 
@@ -74,13 +80,78 @@ def getContinousData():
     data = data[:, :-1]
 
     # z-scoring training data
-    mean = np.mean(data, axis=0)
-    std = np.std(data, axis=0, ddof=1)
-    data = (data - mean) / std
+    if zscore == True:
+        mean = np.mean(data, axis=0)
+        std = np.std(data, axis=0, ddof=1)
+        data = (data - mean) / std
 
     # append classes back onto data
     data = np.append(data, classes, axis=1)
 
     return data
 
-getContinousData()
+
+def getCategoricalData():
+
+    # get the means of the 
+    continuous_data = getContinousData()
+    means = np.mean(continuous_data, axis=0)
+    data = []
+    with open('train.csv') as realtordata:
+
+        # read in data
+        real_read = csv.reader(realtordata)
+        
+        # remove first row and column from data
+        temp_data = []
+        for row in real_read:
+            temp_data.append(row)
+        temp_data = np.array(temp_data)
+        temp_data = temp_data[1:, 1:]
+
+        continuous_indexes = [2, 3, 25, 33, 35, 36, 37, 42, 43, 44, 45, 61, 65, 66, 67, 68, 69, 70, 74]
+
+        # loop through each row of data
+        for row in temp_data:
+            
+            # use temp array to store the update feature values for this row
+            temp = []
+
+            # look through each value in the row
+            for index in range(0, len(row)):
+                
+                # skip the features that contain both integers and strings
+                if index == 2: #LotFrontage
+                    continue
+                if index == 25: # MasVnrArea
+                    continue
+                if index == 58: # GarageYrBlt
+                    continue
+
+                value = row[index]
+
+                # check if the current feature index is equivalent to a continous feature index
+                for cont_index in continuous_indexes:
+                    if index == cont_index:
+                        value = int(value)
+                        if value < means[index]:
+                            value = '0'
+                        else:
+                            value = '1'
+
+                temp.append(value)
+
+
+            # append the new array to the data array
+            data.append(temp)
+
+    # separate classes from data
+    data = np.array(data)
+    classes = data[:, -1:]
+    data = data[:, :-1]
+
+    # append classes back onto data
+    data = np.append(data, classes, axis=1)
+
+    print(data)
+    return data
